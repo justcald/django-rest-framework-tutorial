c1989744efe7b6ece9fbf36c88c169ebeaf2be57
Justin Calder <jcalder@izeni.com>
2016-03-07 17:04:45 -0700

    finishes step 5

--- tutorial/snippets/admin.py
+++ tutorial/snippets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Snippet
+
+
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('code', )
+
+
+admin.site.register(Snippet)
